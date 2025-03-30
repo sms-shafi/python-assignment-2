cc8fa45ae0c3225452edb738e7d133f8b3d4a01b
SHAIK MAHAMMED SHAFI <SMSSHAFI89@GMAIL.COM>
2025-03-31 00:34:59 +0530

    Add files via upload

---
+++ assaignment 2 task 1.py
@@ -0,0 +1,13 @@
+a=int(input('Entar a number: '))
+if (a % 2 != 0):
+    print(a, 'is an odd number.')
+else:
+    print(a, 'is an even number.')
+
+#Another number
+
+a=int(input('Entar a number: '))
+if (a % 2 != 0):
+    print(a, 'is an odd number.')
+else:
+    print(a, 'is an even number.')
